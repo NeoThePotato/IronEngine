7047b2486d13eb7348b6acb4084bc5d1b2a0cd3e
NeoThePorato <neorayb@gmail.com>
2023-05-28 21:48:46 +0300

    Implemented parsing files into character data for reading maps.

--- IO/File.cs
+++ IO/File.cs
@@ -1,10 +1,61 @@
-﻿namespace IO
+﻿using System.Diagnostics;
+using static System.IO.File;
+
+namespace IO
 {
 	static class File
 	{
-		static char[,] LoadMapCharData()
+		public static char[,]? LoadMapCharData(string pathName)
 		{
-			throw new NotImplementedException();
-		}
+			FileStream fs;
+
+            try
+            {
+                fs = Open(pathName, FileMode.Open);
+            }
+			catch (FileNotFoundException)
+			{
+				Debug.WriteLine($"File at{pathName} could not be found.");
+
+				return null;
+			}
+
+            return ParseFileStream(fs);
+        }
+
+		private static char[,] ParseFileStream(FileStream fs)
+        {
+            (int dimJ, int dimI) = GetDimentions(fs);
+            var charData = new char[dimJ, dimI];
+
+            for (int j = 0; j < dimJ; j++)
+            {
+                for (int i = 0; i < dimI; i++)
+                {
+                    char c = (char)fs.ReadByte();
+
+                    if (c == -1)
+                    {
+                        j = dimJ;
+                        break;
+                    }
+                    else if (c == '\n')
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        charData[j, i] = c;
+                    }
+                }
+            }
+
+            return charData;
+        }
+
+        private static (int, int) GetDimentions(FileStream fs)
+        {
+            throw new NotImplementedException();
+        }
 	}
 }
