c90497bd1b5b03e11e9c3ed7892979f3f198a4c7
NeoThePotato <neorayb@gmail.com>
2023-05-28 04:40:52 +0300

    Added namespace IO and a few empty classes.

---
+++ IO/File.cs
@@ -0,0 +1,10 @@
+﻿namespace IO
+{
+	static class File
+	{
+		static char[,] LoadMapCharData()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ IO/PlayerInput.cs
@@ -0,0 +1,7 @@
+﻿namespace IO
+{
+	class PlayerInput
+	{
+
+	}
+}
