a7f6d9ada5fea7426dfcd0a5f6785be60b8aea48
NeoThePotato <neorayb@gmail.com>
2023-05-31 01:23:40 +0300

    Added some functions to GameManager.

--- Game/GameManager.cs
+++ Game/GameManager.cs
@@ -1,10 +1,39 @@
-﻿namespace Game
+﻿using World;
+
+namespace Game
 {
 	class GameManager
 	{
+		public MapManager MapManager { get; private set; }
+
+		public GameManager()
+		{
+			LoadMap("../../../Assets/Maps/TestMap.txt");
+		}
+
 		public void Start()
 		{
+
+		}
+
+		private void Encounter(MapEntity player, MapEntity other)
+		{
 			throw new NotImplementedException();
+		}
+
+		private bool LoadMap(string pathName)
+		{
+			var charData = IO.File.LoadMapCharData(pathName);
+
+			if (charData != null)
+			{
+				MapManager = new MapManager(new Map(charData));
+				return true;
+			}
+			else
+			{
+				return false;
+			}
 		}
 	}
 }
