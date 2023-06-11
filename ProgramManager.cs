7c8cd10e5af42dcda84d517ad1c276d1a8b0be6c
NeoThePotato <neorayb@gmail.com>
2023-06-11 07:01:36 +0300

    Implemented generic frame timing to "ProgramManager".

--- ProgramManager.cs
+++ ProgramManager.cs
@@ -1,10 +1,13 @@
 ﻿using Game;
 using IO.Render;
+using System.Diagnostics;
 
 class ProgramManager
 {
 	public const int GLOBAL_TICK_RATE = 60;
-	public const int FRAMETIME_MILISEC = 1000 / GLOBAL_TICK_RATE;
+	public const double FRAMETIME_MILISEC = 1000.0 / GLOBAL_TICK_RATE;
+	public const long FRAMETIME_TICKS = (long)(TimeSpan.TicksPerMillisecond * FRAMETIME_MILISEC);
+	public readonly TimeSpan FRAMETIME = new TimeSpan(FRAMETIME_TICKS);
 	private GameManager _gameManager;
 	private ConsoleRenderer _consoleRenderer;
 
@@ -19,14 +22,21 @@ class ProgramManager
 	{
 		while (true)
 		{
+			DateTime startTime = DateTime.Now;
+
 			// Logic
 			_gameManager.Update();
+			DateTime logicTime = DateTime.Now;
 
-			// Visuals
+			// Render
 			_consoleRenderer.RenderFrame();
+			DateTime renderTime = DateTime.Now;
 
-			// TODO Make this use time delta instead of a fixed time.
-			Thread.Sleep(FRAMETIME_MILISEC);
+			// Frame Timing
+			TimeSpan totalUpdateTime = renderTime - startTime;
+			TimeSpan timeDelta = FRAMETIME > totalUpdateTime ? FRAMETIME - totalUpdateTime : new TimeSpan(0);
+			Debug.WriteLine($"Logic Time: {(logicTime-startTime).Milliseconds}ms\nRender Time: {(renderTime - logicTime).Milliseconds}ms\nTotal update time: {totalUpdateTime}");
+			Thread.Sleep(timeDelta);
 		}
 	}
 }
