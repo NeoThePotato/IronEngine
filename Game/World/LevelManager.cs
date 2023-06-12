c3d2aca0c552560322cf123f6b2623966161494e
NeoThePorato <neorayb@gmail.com>
2023-06-12 12:38:45 +0300

    Added direction tracking to "MapEntity".

--- Game/World/LevelManager.cs
+++ Game/World/LevelManager.cs
@@ -1,47 +1,47 @@
 ﻿namespace Game.World
 {
-    class LevelManager
-    {
-        private Map _map;
-        private List<MapEntity> _entities;
+	class LevelManager
+	{
+		private Map _map;
+		private List<MapEntity> _entities;
 
-        public Map Map
-        { get => _map; private set => _map = value; }
-        public List<MapEntity> Entities
-        { get => _entities; private set => _entities = value; }
+		public Map Map
+		{ get => _map; private set => _map = value; }
+		public List<MapEntity> Entities
+		{ get => _entities; private set => _entities = value; }
 
-        public LevelManager(Map map)
-        {
-            _map = map;
-            _entities = new List<MapEntity>();
-        }
+		public LevelManager(Map map)
+		{
+			_map = map;
+			_entities = new List<MapEntity>();
+		}
 
-        public MapEntity AddEntity(Entity entity, int posJ, int posI)
-        {
-            var mapEntity = new MapEntity(entity, posJ, posI);
-            AddEntity(mapEntity);
+		public MapEntity AddEntity(Entity entity, int posJ, int posI)
+		{
+			var mapEntity = new MapEntity(entity, posJ, posI);
+			AddEntity(mapEntity);
 
-            return mapEntity;
-        }
+			return mapEntity;
+		}
 
-        public void AddEntity(MapEntity entity)
-        {
-            Entities.Add(entity);
-        }
+		public void AddEntity(MapEntity entity)
+		{
+			Entities.Add(entity);
+		}
 
-        public bool MoveEntity(MapEntity entity, Direction.Directions direction, out MapEntity? otherEntity)
-        {
-            if (CanEntityMoveTo(entity, direction, out otherEntity))
-            {
-                entity.Move(direction);
+		public bool MoveEntity(MapEntity entity, Direction.Directions direction, out MapEntity? otherEntity)
+		{
+			if (CanEntityMoveTo(entity, direction, out otherEntity))
+			{
+				entity.Move(direction);
 
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
 
 		private bool CanEntityMoveTo(MapEntity entity, Direction.Directions direction, out MapEntity? occupiedBy)
 		{
@@ -50,32 +50,32 @@
 			occupiedBy = null;
 
 			if (TileTraversable(newPosJ, newPosI))
-            {
-                if (TileOccupied(newPosJ, newPosI, out occupiedBy))
-                {
-                    if (occupiedBy == entity)
-                    {
-                        occupiedBy = null;
-                        return true;
-                    }
-                    else
-                    {
-                        return false;
-                    }
-                }
-                else
-                {
-                    return true;
-                }
-            }
-            else
-            {
-                return false;
-            }
+			{
+				if (TileOccupied(newPosJ, newPosI, out occupiedBy))
+				{
+					if (occupiedBy == entity)
+					{
+						occupiedBy = null;
+						return true;
+					}
+					else
+					{
+						return false;
+					}
+				}
+				else
+				{
+					return true;
+				}
+			}
+			else
+			{
+				return false;
+			}
 		}
 
 		private bool TileTraversable(int posJ, int posI)
-        {
+		{
 			return !(TileOutOfBounds(posJ, posI) || TileImpassable(posJ, posI));
 		}
 
@@ -85,76 +85,85 @@
 		}
 
 		private bool TileImpassable(int posJ, int posI)
-        {
-            return !Map.GetTileInfo(posJ, posI).passable;
-        }
+		{
+			return !Map.GetTileInfo(posJ, posI).passable;
+		}
 
-        private bool TileOccupied(int posJ, int posI, out MapEntity? occupiedBy)
-        {
+		private bool TileOccupied(int posJ, int posI, out MapEntity? occupiedBy)
+		{
 			occupiedBy = GetEntityAt(posJ, posI);
 
 			return occupiedBy != null;
-        }
+		}
 
-        private MapEntity? GetEntityAt(int posJ, int posI)
-        {
-            foreach (var entity in Entities)
-            {
-                if (entity.PosJ == posJ && entity.PosI == posI)
-                    return entity;
-            }
+		private MapEntity? GetEntityAt(int posJ, int posI)
+		{
+			foreach (var entity in Entities)
+			{
+				if (entity.PosJ == posJ && entity.PosI == posI)
+					return entity;
+			}
 
-            return null;
-        }
-    }
+			return null;
+		}
+	}
 
-    class MapEntity
-    {
-        public Entity Entity
-        { get; set; }
-        public int PosJ
-        { get; set; }
+	class MapEntity
+	{
+		public Entity Entity
+		{ get; set; }
+		public int PosJ
+		{ get; set; }
 		public int PosI
 		{ get; set; }
+		public Direction.Directions Dir
+		{ get; set; }
 
-		public MapEntity(Entity entity, int posJ, int posI)
+		public MapEntity(Entity entity, int posJ, int posI, Direction.Directions direction = Direction.Directions.None)
+		{
+			Entity = entity;
+			PosJ = posJ;
+			PosI = posI;
+			Dir = direction;
+		}
+
+        public void Move()
         {
-            Entity = entity;
-            PosJ = posJ;
-            PosI = posI;
+            Move(Dir);
         }
 
         public void Move(Direction.Directions direction)
-        {
-            (int movJ, int movI) = Direction.TranslateDirection(direction);
-            Move(movJ, movI);
-        }
+		{
+			(int movJ, int movI) = Direction.TranslateDirection(direction);
+			Dir = direction;
+			Move(movJ, movI);
+		}
 
-        public void Move(int movJ, int movI)
-        {
-            PosJ += movJ;
-            PosI += movI;
-        }
-    }
+		public void Move(int movJ, int movI)
+		{
+			PosJ += movJ;
+			PosI += movI;
+		}
+	}
 
-    static class Direction
-    {
-        private static readonly (int, int)[] DIRECTION_VECTORS = {
-            (0, 1),
-            (-1, 1),
-            (-1, 0),
-            (-1, -1),
-            (0, -1),
-            (1, -1),
-            (1, 0),
-            (1, 1),
-            (0, 0)
-        };
+	static class Direction
+	{
+		private static readonly (int, int)[] DIRECTION_VECTORS = {
+			(0, 1),
+			(-1, 1),
+			(-1, 0),
+			(-1, -1),
+			(0, -1),
+			(1, -1),
+			(1, 0),
+			(1, 1),
+			(0, 0)
+		};
 
-        public static (int, int) TranslateDirection(Directions direction)
-        {
-            return DIRECTION_VECTORS[(int)direction];
-        }
+		public static (int, int) TranslateDirection(Directions direction)
+		{
+			return DIRECTION_VECTORS[(int)direction];
+		}
 
 		public static Directions TranslateDirection((int, int) vector)
 		{
@@ -162,16 +171,16 @@
 		}
 
 		public enum Directions
-        {
-            E = 0,
-            NE = 1,
-            N = 2,
-            NW = 3,
-            W = 4,
-            SW = 5,
-            S = 6,
-            SE = 7,
-            None = 8
-        }
-    }
+		{
+			E = 0,
+			NE = 1,
+			N = 2,
+			NW = 3,
+			W = 4,
+			SW = 5,
+			S = 6,
+			SE = 7,
+			None = 8
+		}
+	}
 }
