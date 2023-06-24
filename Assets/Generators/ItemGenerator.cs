686ae803c43c8b56b76ce9db887c536f4aa9ecc4
NeoThePotato <neorayb@gmail.com>
2023-06-25 01:19:12 +0300

    Expanded "ItemGenerator"'s list of spawnable items.

--- Assets/Generators/ItemGenerator.cs
+++ Assets/Generators/ItemGenerator.cs
@@ -6,12 +6,20 @@ namespace Assets.Generators
 	{
 		private static readonly Dictionary<Item, SpawnProfile> SPAWNABLE_ITEMS = new Dictionary<Item, SpawnProfile>()
 		{
-			{EquipmentTemplates.Weapons.rustedBlade,	new SpawnProfile(1, 5)},
-/*			{Item.bandit,			new SpawnProfile(2, 5)},
-			{Item.imp,				new SpawnProfile(2, 6)},
-			{Item.fae,				new SpawnProfile(2, 6)},
-			{Item.spawnOfTwilight,	new SpawnProfile(4, 2)},
-			{Item.antiHero,			new SpawnProfile(10, 2)},*/
+			{EquipmentTemplates.Weapons.rustedBlade,		new SpawnProfile(1, 5)},
+			{EquipmentTemplates.Weapons.steelSword,			new SpawnProfile(2, 4)},
+			{EquipmentTemplates.Weapons.umbraSword,			new SpawnProfile(5, 3)},
+			{EquipmentTemplates.Weapons.swordExcalibur,		new SpawnProfile(10, 2)},
+			{EquipmentTemplates.Armors.rustedBuckler,		new SpawnProfile(1, 5)},
+			{EquipmentTemplates.Armors.steelBuckler,		new SpawnProfile(2, 4)},
+			{EquipmentTemplates.Armors.towerShield,			new SpawnProfile(5, 3)},
+			{EquipmentTemplates.Armors.heroShield,			new SpawnProfile(10, 2)},
+			{EquipmentTemplates.Armors.rustedChestplate,    new SpawnProfile(1, 5)},
+			{EquipmentTemplates.Armors.tatteredRags,		new SpawnProfile(1, 5)},
+			{EquipmentTemplates.Armors.blackRobes,			new SpawnProfile(2, 4)},
+			{EquipmentTemplates.Armors.leatherArmor,		new SpawnProfile(3, 3)},
+			{EquipmentTemplates.Armors.moltenArmor,			new SpawnProfile(5, 2)},
+			{EquipmentTemplates.Armors.mithrilChainmail,	new SpawnProfile(10, 1)},
 		};
 
 		public static Item MakeItem(int level)
