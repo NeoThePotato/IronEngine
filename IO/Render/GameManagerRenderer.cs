26f6f9fdc2a2d2638a7a8f8edd667439a1fc12f4
NeoThePotato <neorayb@gmail.com>
2023-06-25 14:15:18 +0300

    Made "GameManagerRenderer" rendering borders non-static.

--- IO/Render/GameManagerRenderer.cs
+++ IO/Render/GameManagerRenderer.cs
@@ -6,8 +6,6 @@ namespace IO.Render
 	{
 		private readonly (char, byte, byte) BORDER_INFO_V = ('║', 15, 0);
 		private readonly (char, byte, byte) BORDER_INFO_H = ('═', 15, 0);
-		private int[] _borderLinesJ;
-		private int[] _borderLinesI;
 
 		public GameManager GameManager
 		{ get; private set; }
@@ -16,17 +14,21 @@ namespace IO.Render
 		public GameUIManagerRenderer UIManagerRenderer
 		{ get; private set; }
 		public override int SizeJ
-		{ get => _borderLinesJ.Length + LevelManagerRenderer.SizeJ + GameManager.DataLog.MAX_SIZE; }
+		{ get => BorderLinesJ.Length + LevelManagerRenderer.SizeJ + GameManager.DataLog.MAX_SIZE; }
 		public override int SizeI
-		{ get => _borderLinesI.Length + LevelManagerRenderer.SizeI; }
+		{ get => BorderLinesI.Length + LevelManagerRenderer.SizeI; }
+		private int[] BorderLinesJ
+		{ get; }
+		private int[] BorderLinesI
+		{ get; }
 
 		public GameManagerRenderer(GameManager gameManager)
 		{
 			GameManager = gameManager;
 			UIManagerRenderer = new GameUIManagerRenderer(GameManager.UIManager, this);
-			LevelManagerRenderer = new LevelManagerRenderer(GameManager.LevelManager, UIManagerRenderer);
-			_borderLinesJ = new int[] {0, LevelManagerRenderer.SizeJ + 1, LevelManagerRenderer.SizeJ + GameManager.UIManager.DataLog.MAX_SIZE + 2 };
-			_borderLinesI = new int[] {0, LevelManagerRenderer.SizeI + 1};
+			LevelManagerRenderer = new LevelManagerRenderer(GameManager);
+			BorderLinesJ = new int[3];
+			BorderLinesI = new int[2];
 		}
 
 		public override void Render(FrameBuffer buffer)
@@ -45,7 +47,7 @@ namespace IO.Render
 		private void RenderBorders(FrameBuffer buffer)
 		{
 			// Render vertical borders
-			foreach (var border in _borderLinesI)
+			foreach (var border in BorderLinesI)
 			{
 				for (int j = 0; j < SizeJ; j++)
 				{
@@ -54,7 +56,7 @@ namespace IO.Render
 			}
 
 			// Render horizontal borders
-			foreach (var border in _borderLinesJ)
+			foreach (var border in BorderLinesJ)
 			{
 				for (int i = 0; i < SizeI; i++)
 				{
@@ -63,12 +65,21 @@ namespace IO.Render
 			}
 
 			// Corners
-			buffer.Char[_borderLinesJ[0], _borderLinesI[0]] = '╔';
-			buffer.Char[_borderLinesJ[1], _borderLinesI[0]] = '╠';
-			buffer.Char[_borderLinesJ[2], _borderLinesI[0]] = '╚';
-			buffer.Char[_borderLinesJ[0], _borderLinesI[1]] = '╗';
-			buffer.Char[_borderLinesJ[1], _borderLinesI[1]] = '╣';
-			buffer.Char[_borderLinesJ[2], _borderLinesI[1]] = '╝';
+			buffer.Char[BorderLinesJ[0], BorderLinesI[0]] = '╔';
+			buffer.Char[BorderLinesJ[1], BorderLinesI[0]] = '╠';
+			buffer.Char[BorderLinesJ[2], BorderLinesI[0]] = '╚';
+			buffer.Char[BorderLinesJ[0], BorderLinesI[1]] = '╗';
+			buffer.Char[BorderLinesJ[1], BorderLinesI[1]] = '╣';
+			buffer.Char[BorderLinesJ[2], BorderLinesI[1]] = '╝';
+		}
+
+		private void ValidateBorderSizes()
+		{
+			BorderLinesJ[0] = 0;
+			BorderLinesJ[1] = LevelManagerRenderer.SizeJ + 1;
+			BorderLinesJ[2] = LevelManagerRenderer.SizeJ + GameManager.UIManager.DataLog.MAX_SIZE + 2;
+			BorderLinesI[0] = 0;
+			BorderLinesI[1] = LevelManagerRenderer.SizeI + 1;
 		}
 	}
 }
