2d17f4e6ec2b60e9a5e11d54a91109c069bd64f1
NeoThePotato <neorayb@gmail.com>
2023-06-30 21:05:16 +0300

    Added another sidebar for player XP.

--- IO/Render/GameUIManagerRenderer.cs
+++ IO/Render/GameUIManagerRenderer.cs
@@ -1,5 +1,6 @@
 ﻿using Game;
 using Game.Combat;
+using Game.Progression;
 using IO.UI;
 
 namespace IO.Render
@@ -7,7 +8,9 @@ namespace IO.Render
 	class GameUIManagerRenderer : Renderer
 	{
 		public const int SIDEBAR_WIDTH = 10;
+		public const int METER_WIDTH = 2;
 		public static readonly byte HP_COLOR = 1;
+		public static readonly byte XP_COLOR = 11;
 		public static readonly byte BLANK_COLOR = 0;
 
 		private GameUIManager UIManager
@@ -55,13 +58,21 @@ namespace IO.Render
 		{
 			var sideBarBuffer = new FrameBuffer(buffer, 1, GameManagerRenderer.LevelManagerRenderer.SizeI + 2);
 			RenderPlayerHP(sideBarBuffer);
+			RenderPlayerExp(new FrameBuffer(sideBarBuffer, 0, METER_WIDTH));
 		}
 
 		private void RenderPlayerHP(FrameBuffer buffer)
 		{
 			Unit player = (Unit)GameManager.LevelManager.PlayerEntity.Entity;
 			float playerHPPercent = (float)player.CurrentHP / player.MaxHP;
-			RenderMeter(buffer, playerHPPercent, HP_COLOR, BLANK_COLOR, "HP", 2);
+			RenderMeter(buffer, playerHPPercent, HP_COLOR, BLANK_COLOR, "HP", METER_WIDTH);
+		}
+
+		private void RenderPlayerExp(FrameBuffer buffer)
+		{
+			Unit player = (Unit)GameManager.LevelManager.PlayerEntity.Entity;
+			float playerXPPercent = 1f - ((float)player.ExpToNextLevel / Leveling.GetExpAtLevel(player.Level+1));
+			RenderMeter(buffer, playerXPPercent, XP_COLOR, BLANK_COLOR, "XP", METER_WIDTH);
 		}
 
 		private void RenderMeter(FrameBuffer buffer, float percentage, byte fullColor, byte emptyColor, string text, int length = 1)
@@ -76,7 +87,7 @@ namespace IO.Render
 				for (int i = 0; i < length; i++)
 					buffer.Background[j, i] = color;
 			}
-			RenderText(new FrameBuffer(buffer, SideBarSizeJ-1), "HP");
+			RenderText(new FrameBuffer(buffer, SideBarSizeJ-1), text);
 		}
 	}
 }
