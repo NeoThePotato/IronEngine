4c2fa8d5ee42ee205c48a07c28e667f5ab7cafc7
NeoThePotato <neorayb@gmail.com>
2023-07-01 19:02:19 +0300

    Added some poggers new maps. :) (And scrapped some old ones).

--- Assets/MapTemplates.cs
+++ Assets/MapTemplates.cs
@@ -8,11 +8,23 @@ namespace Assets
 
 		public static readonly Dictionary<string, MapMetadata> MAPS_DICTIONARY = new Dictionary< string, MapMetadata>()
 		{
-			{ "TestMap",		new MapMetadata("Test Map",		$"{MAPS_PATH}TestMap.txt",		Point2D.Tile(15, 2),	Point2D.Tile(2, 36))},
-			{ "Palace",			new MapMetadata("Palace",		$"{MAPS_PATH}Palace.txt",		Point2D.Tile(102, 49),	Point2D.Tile(4, 90))},
-			{ "PalaceSmall",	new MapMetadata("Palace",		$"{MAPS_PATH}PalaceSmall.txt",	Point2D.Tile(59, 49),	Point2D.Tile(4, 90))},
-			{ "Ship",			new MapMetadata("Ship",			$"{MAPS_PATH}Ship.txt",			Point2D.Tile(9, 23),	Point2D.Tile(70, 15))},
-			{ "CastleRuins1",	new MapMetadata("Castle Ruins",	$"{MAPS_PATH}CastleRuins1.txt",	Point2D.Tile(74, 59),	Point2D.Tile(21, 30))},
+			{ "Plains1",		new MapMetadata("Rocky Plains",			$"{MAPS_PATH}Plains1.txt",		Point2D.Tile(0, 50),	Point2D.Tile(37, 80))},
+			{ "Plains2",		new MapMetadata("Suspicious Structure",	$"{MAPS_PATH}Plains2.txt",		Point2D.Tile(29, 29),	Point2D.Tile(0, 26))},
+			{ "Village1",		new MapMetadata("Fancy village",		$"{MAPS_PATH}Village1.txt",		Point2D.Tile(28, 3),	Point2D.Tile(4, 43))},
+			{ "Village2",		new MapMetadata("Quiet village",		$"{MAPS_PATH}Village2.txt",     Point2D.Tile(30, 25),	Point2D.Tile(2, 47))},
+			{ "Village3",		new MapMetadata("Moat-Side village",	$"{MAPS_PATH}Village3.txt",		Point2D.Tile(29, 23),	Point2D.Tile(0, 23))},
+			{ "Palace1",		new MapMetadata("The Grand Palace",		$"{MAPS_PATH}Palace1.txt",		Point2D.Tile(41, 48),	Point2D.Tile(6, 97))},
+			{ "Sewers1",		new MapMetadata("Sewers Cistern",		$"{MAPS_PATH}Sewers1.txt",		Point2D.Tile(42, 50),	Point2D.Tile(0, 50))},
+			{ "Sewers2",		new MapMetadata("Sewers North",			$"{MAPS_PATH}Sewers2.txt",		Point2D.Tile(13, 24),	Point2D.Tile(13, 26))},
+			{ "Sewers3",		new MapMetadata("Sewers South",			$"{MAPS_PATH}Sewers3.txt",		Point2D.Tile(1, 33),	Point2D.Tile(23, 32))},
+			{ "Ship1",			new MapMetadata("Ship",					$"{MAPS_PATH}Ship1.txt",		Point2D.Tile(6, 15),	Point2D.Tile(45, 7))},
+		};
+
+		public static readonly Dictionary<string, MapMetadata> CUT_MAPS_DICTIONARY = new Dictionary<string, MapMetadata>()
+		{
+			{ "TestMap",		new MapMetadata("Test Map",			$"{MAPS_PATH}TestMap.txt",		Point2D.Tile(15, 2),	Point2D.Tile(2, 36))},
+			{ "Palace",			new MapMetadata("Palace",			$"{MAPS_PATH}Palace.txt",		Point2D.Tile(102, 49),	Point2D.Tile(4, 90))},
+			{ "CastleRuins1",	new MapMetadata("Castle Ruins",		$"{MAPS_PATH}CastleRuins1.txt",	Point2D.Tile(74, 59),	Point2D.Tile(21, 30))},
 		};
 
 		public static MapMetadata GetRandomMapMeta()
--- Assets/Tiles.cs
+++ Assets/Tiles.cs
@@ -5,21 +5,23 @@ namespace Assets
 {
 	static class Tiles
 	{
-		public static readonly Dictionary<char, TileInfo> TILE_INFO = new Dictionary<char, TileInfo>(){
+		public static readonly Dictionary<char, TileInfo> TILE_INFO = new Dictionary<char, TileInfo>()
+		{
 			{'?', new TileInfo("Missing TILE_INFO", false)},
 			{' ', new TileInfo("Ground", true)},
 			{'s', new TileInfo("Stone Brick Floor", true)},
 			{'S', new TileInfo("Stone Brick Wall", false)},
 			{'g', new TileInfo("Grass", true)},
-			{'r', new TileInfo("Rock Ground", true)},
+			{'r', new TileInfo("Road", true)},
 			{'R', new TileInfo("Rock Wall", false)},
 			{'w', new TileInfo("Water (Shallow)", true)},
 			{'W', new TileInfo("Water (Deep)", false)},
 			{'p', new TileInfo("Wooden Plank", true)},
 			{'P', new TileInfo("Wooden Plank Wall", false)}
-			};
+		};
 
-		public static readonly Dictionary<char, VisualTileInfo> VISUAL_TILE_INFO = new Dictionary<char, VisualTileInfo>(){
+		public static readonly Dictionary<char, VisualTileInfo> VISUAL_TILE_INFO = new Dictionary<char, VisualTileInfo>()
+		{
 			{'?', new VisualTileInfo(TileInfo.GetTileInfo('?'), '▒', 163, 0)},
 			{' ', new VisualTileInfo(TileInfo.GetTileInfo(' '), ' ', 15, 0)},
 			{'s', new VisualTileInfo(TileInfo.GetTileInfo('s'), '▓', 244, 239)},
@@ -31,6 +33,6 @@ namespace Assets
 			{'W', new VisualTileInfo(TileInfo.GetTileInfo('W'), '≈', 33, 32)},
 			{'p', new VisualTileInfo(TileInfo.GetTileInfo('p'), '≡', 137, 180)},
 			{'P', new VisualTileInfo(TileInfo.GetTileInfo('P'), '▒', 52, 137)},
-			};
+		};
 	}
 }
