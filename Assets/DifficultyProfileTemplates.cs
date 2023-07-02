200ccf6a45dffe87ebba223e44ad5b678ef59ed3
NeoThePotato <neorayb@gmail.com>
2023-07-02 06:12:33 +0300

    Added the final boss which spawns at level 15.

--- Assets/DifficultyProfileTemplates.cs
+++ Assets/DifficultyProfileTemplates.cs
@@ -4,6 +4,6 @@ namespace Assets
 {
 	static class DifficultyProfileTemplates
 	{
-		public static DifficultyProfile normalDifficulty = new DifficultyProfile("Normal", 250, 500, 0.5f, 2, 0.25f, 3);
+		public static DifficultyProfile normalDifficulty = new DifficultyProfile("Normal", 250, 500, 0.5f, 2, 0.25f, 3, 15);
 	}
 }
