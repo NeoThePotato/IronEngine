af4d3e998320d7c3339ad339fd880423f0784b85
NeoThePotato <neorayb@gmail.com>
2024-04-21 13:19:38 +0300

    Add optional TileObject to Tile.

--- Core/Tile.cs
+++ Core/Tile.cs
@@ -1,8 +1,14 @@
-﻿namespace IronEngine
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace IronEngine
 {
 	public class Tile
 	{
 		public Position Position { get; private set; }
+
+		public TileObject? Object { get; private set; }
+
+		public bool HasObject => Object != null;
 
 		#region EVENTS
 		public event Action<Tile, TileObject> OnObjectEnter;
