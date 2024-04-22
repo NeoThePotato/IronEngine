93f19da2ca1813d34adf2eab9b0db27117e96a89
NeoThePotato <neorayb@gmail.com>
2024-04-22 16:29:25 +0300

    Make Tile implement ICloneable.

--- Core/Tile.cs
+++ Core/Tile.cs
@@ -2,12 +2,12 @@
 
 namespace IronEngine
 {
-	public class Tile : IPositionable, IHasActor
+	public class Tile : ICloneable, IPositionable, IHasActor
 	{
 		[NotNull]
-		public TileMap TileMap { get; internal init; }
+		public TileMap TileMap { get; internal set; }
 
-		public Position Position { get; internal init; }
+		public Position Position { get; internal set; }
 
 		public TileObject? Object { get; internal set; }
 
@@ -19,6 +19,17 @@ namespace IronEngine
 		public event Action<Tile, TileObject> OnObjectEnter;
 		public event Action<Tile, TileObject> OnObjectExit;
 		#endregion
+
+		public object Clone() => CloneDeep();
+
+		public virtual Tile CloneDeep()
+		{
+			var clone = Utilities.CloneShallow(this);
+			clone.Position = Position.OutOfBounds;
+			clone.TileMap = null;
+			clone.Object = Object?.CloneDeep();
+			return clone;
+		}
 
 		public bool SameTileMap(Tile other) => TileMap != null && TileMap == other.TileMap;
 	}
