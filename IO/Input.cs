d487b32e4125c7376ba388cd84e8044c362a5931
NeoThePotato <neorayb@gmail.com>
2024-05-07 14:17:15 +0300

    Add IInput function for getting a string prompt.

--- IO/Input.cs
+++ IO/Input.cs
@@ -2,6 +2,8 @@
 {
 	public interface IInput
 	{
+		public string GetString(string prompt);
+
 		public IActionable PickActionable(IEnumerable<IActionable> actionables);
 
 		public IActionable.Action PickAction(IEnumerable<IActionable.Action> actions);
