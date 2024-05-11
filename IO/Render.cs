6c14274ca708a3cdda6881f5424dc6a464ce9a13
NeoThePotato <neorayb@gmail.com>
2024-05-11 13:12:12 +0300

    Trim down the IRenderer and IRenderAble interfaces.

--- IO/Render.cs
+++ IO/Render.cs
@@ -3,16 +3,10 @@
 	public interface IRenderer
 	{
 		void UpdateFrame();
-
-		void UpdateFrameFull();
-
-		void WriteLine(string str);
 	}
 
 	public interface IRenderAble
 	{
-		bool isStatic { get; }
-
-		void Render();
+		IRenderer GetRenderer();
 	}
 }
