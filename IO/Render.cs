abb1157f163259c047f81bbb4ded0e92f09fe498
NeoThePotato <neorayb@gmail.com>
2024-05-12 23:28:59 +0300

    Document interfaces IRenderer and IRenderAble.

--- IO/Render.cs
+++ IO/Render.cs
@@ -1,12 +1,22 @@
 ﻿namespace IronEngine.IO
 {
+	/// <summary>
+	/// Interface for an object that can render instances of <see cref="IRenderAble"/>.
+	/// </summary>
 	public interface IRenderer
 	{
+		/// <summary>
+		/// Update the frame.
+		/// </summary>
 		void UpdateFrame();
 	}
 
+	/// <summary>
+	/// Interface for an object that can be rendered using <see cref="IRenderer"/>.
+	/// </summary>
 	public interface IRenderAble
 	{
+		/// <returns>The <see cref="IRenderer"/> instance responsible for rendering this object.</returns>
 		IRenderer GetRenderer();
 	}
 }
