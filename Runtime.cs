9f9e8083b0573a3322fa68d621a14988a841c2fa
NeoThePotato <neorayb@gmail.com>
2024-05-12 23:44:31 +0300

    Document class Runtime.

--- Runtime.cs
+++ Runtime.cs
@@ -3,6 +3,11 @@ using IronEngine.IO;
 
 namespace IronEngine
 {
+	/// <summary>
+	/// This class handles the core of the engine's runtime.
+	/// Implement this to create your game's rules.
+	/// Call <see cref="Run"/> to run this.
+	/// </summary>
 	public abstract class Runtime : IDestroyable
 	{
 		#region SINGLETON
@@ -11,12 +16,23 @@ namespace IronEngine
 		#endregion
 
 		#region TILEMAP
+		/// <summary>
+		/// The <see cref="TileMap"/> which is managed by this <see cref="Runtime"/>.
+		/// </summary>
 		protected TileMap TileMap { get; private set; }
 		#endregion
 
 		#region ACTORS
 		private List<Actor> _actors;
+
+		/// <summary>
+		/// The <see cref="Actor"/> which is currently acting.
+		/// </summary>
 		protected Actor CurrentActor => _turnCounter.Current;
+
+		/// <summary>
+		/// All <see cref="Actor"/>s which the <see cref="Runtime"/> currently manages.
+		/// </summary>
 		protected IEnumerable<Actor> Actors => _actors;
 
 		internal bool RemoveActor(Actor actor)
@@ -30,6 +46,9 @@ namespace IronEngine
 		#region PLAYER
 		private TurnEnumerator _turnCounter;
 
+		/// <summary>
+		/// Current turn count.
+		/// </summary>
 		protected uint Turn => _turnCounter._turnCounter;
 
 		public Runtime()
@@ -41,6 +60,9 @@ namespace IronEngine
 			_turnCounter = new(_actors);
 		}
 
+		/// <summary>
+		/// Entry point for running this <see cref="Runtime"/>.
+		/// </summary>
 		public void Run()
 		{
 			OnGameStart();
@@ -73,6 +95,9 @@ namespace IronEngine
 
 		private void PlayerExit() => OnExit();
 
+		/// <summary>
+		/// Destroys this <see cref="Runtime"/> and release all managed resources.
+		/// </summary>
 		public void Destroy()
 		{
 			TileMap.Destroy();
@@ -81,22 +106,53 @@ namespace IronEngine
 		#endregion
 
 		#region RENDERER
+		/// <summary>
+		/// The <see cref="IRenderer"/> responsible for rendering frames to the user.
+		/// </summary>
 		protected IRenderer Renderer { get; private set; }
 		#endregion
 
 		#region ABSTRACT
+		/// <summary>
+		/// Condition for the <see cref="Runtime"/> to finish executing.
+		/// </summary>
 		protected abstract bool ExitCondition { get; }
 
+		/// <summary>
+		/// The <see cref="IInput"/> responsible for receiving input from the <see cref="Actor"/>s.
+		/// </summary>
 		protected abstract IInput Input { get; }
 
+		/// <summary>
+		/// Called when the <see cref="Runtime"/> is constructed.
+		/// Implement this to create <see cref="Actor"/>s for the <see cref="Runtime"/> to manage.
+		/// </summary>
+		/// <returns>The created <see cref="Actor"/>s for the <see cref="Runtime"/> to manage.</returns>
 		protected abstract IEnumerable<Actor> CreateActors();
 
+		/// <summary>
+		/// Called when the <see cref="Runtime"/> is constructed.
+		/// Implement this to create a <see cref="TileMap"/> for the <see cref="Runtime"/> to manage.
+		/// </summary>
+		/// <returns>The created <see cref="TileMap"/> for the <see cref="Runtime"/> to manage.</returns>
 		protected abstract TileMap CreateTileMap();
 
+		/// <summary>
+		/// Called when the <see cref="Runtime"/> is constructed.
+		/// Implement this to create a <see cref="IRenderer"/> for the <see cref="Runtime"/> to manage.
+		/// </summary>
+		/// <returns>The created <see cref="IRenderer"/> for the <see cref="Runtime"/> to manage.</returns>
 		protected abstract IRenderer CreateRenderer();
 
+		/// <summary>
+		/// Callback for when the game starts.
+		/// </summary>
+		/// <seealso cref="Run"/>
 		protected virtual void OnGameStart() { }
 
+		/// <summary>
+		/// Callback for when the game ends.
+		/// </summary>
 		protected abstract void OnExit();
 		#endregion
 
